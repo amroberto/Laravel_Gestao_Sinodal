499693e79733371786c03d4c9dbda0a230deffb2
Alexandre Martins Roberto <amroberto@gmail.com>
2024-02-09 21:07:14 -0300

    Model and migration created and config for Cidade and Grupo

---
+++ database/migrations/2024_02_09_235421_create_cidades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('codigo_ibge');
+            $table->unsignedBigInteger('estado_id');
+            $table->timestamps();
+
+            // foreign key (constraints)
+            $table->foreign('estado_id')->references('id')->on('estados');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cidades');
+    }
+};
